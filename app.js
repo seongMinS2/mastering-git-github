b878d1b14d7fbfd432978368fec3479059115a36
DevMin <webdevmin@gmail.com>
2022-01-06 12:45:04 +0900

    Change the title of main page

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+    res.status(404).send('Sorry cant find that!');
+  });
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
